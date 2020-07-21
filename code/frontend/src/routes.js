edd1ff71ee7ef1109583cc608c1104a9466bda69
권남선 <namseon1120@naver.com>
2020-07-21 12:30:36 +0000

    Feature/mypage/fe

--- code/frontend/src/routes.js
+++ code/frontend/src/routes.js
@@ -1,39 +1,37 @@
-import Login from "./views/user/Login.vue";
-import Join from "./views/user/Join.vue";
-import FeedMain from "./views/feed/IndexFeed.vue";
-import Components from "./views/Components.vue";
-import SearchPassword from "./components/common/SearchPassword.vue";
-import CheckCertification from "./components/common/CheckCertification";
+
+
+import Login from './views/user/Login.vue'
+import Join from './views/user/Join.vue'
+import Mypage from './views/user/Mypage.vue'
+import FeedMain from './views/feed/IndexFeed.vue'
+import Components from './views/Components.vue'
 
 export default [
-  {
-    path: "/",
-    name: "Login",
-    component: Login,
-  },
-  {
-    path: "/user/join",
-    name: "Join",
-    component: Join,
-  },
-  {
-    path: "/feed/main",
-    name: "FeedMain",
-    component: FeedMain,
-  },
-  {
-    path: "/components",
-    name: "Components",
-    component: Components,
-  },
-  {
-    path: "/user/searchpassword",
-    name: "SearchPassword",
-    component: SearchPassword,
-  },
-  {
-    path: "/user/checkcertification",
-    name: "CheckCertification",
-    component: CheckCertification,
-  },
-];
+
+
+    {
+        path : '/',
+        name : 'Login',
+        component : Login
+    },
+    {
+        path : '/user/join',
+        name : 'Join',
+        component : Join
+    },
+    {
+        path : '/user/mypage',
+        name : 'Mypage',
+        component : Mypage
+    },
+    {
+        path : '/feed/main',
+        name : 'FeedMain',
+        component : FeedMain
+    },
+    {
+        path : '/components',
+        name : 'Components',
+        component : Components
+    }
+]
---
+++ code/frontend/src/views/user/Mypage.vue
@@ -0,0 +1,74 @@
+<template>
+  <div class="user">
+    <div class="mypage">
+      <div class="header">Header</div>
+      <div class="mypage-body">
+        <div class="profil">
+          <div class="myphoto"><v-avatar size="100"><img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John"></v-avatar></div>
+          <div class="myprofil">
+            <div style="margin: 10px">
+              <h2 class="user-name">사용자 이름</h2>
+              <span class="myprofil-icon"><i class="fas fa-cog"></i></span>
+            </div>
+            <v-container class="grey lighten-5" style="min-height: 0; padding: 10px" >
+              <v-row class="myprofil-boxes" no-gutters>
+                <v-col class="myprofil-box" cols="4">
+                  <span>레시피 수</span>
+                  <h2>5</h2>
+                </v-col>
+                <v-col class="myprofil-box" cols="4">
+                  <span>팔로우</span>
+                  <h2>32</h2>
+                </v-col>
+                <v-col class="myprofil-box" cols="4" style="border-right: 1px solid black">
+                  <span>팔로잉</span>
+                  <h2>49</h2>
+                </v-col>
+              </v-row>
+            </v-container>
+          </div>
+        </div>
+        <div class="myfeed">
+          <div class="myprofil-feed">
+              <div class="myprofil-text">
+                <img src="../../assets/images/pencil.png" alt="my recipe">
+                <!-- <i class="fas fa-pencil-alt fa-lg"></i> -->
+                <h3>내가 쓴 글</h3>
+              </div>
+          </div>
+          <div class="myprofil-scrap">
+            <div class="myprofil-text">
+              <img src="../../assets/images/bookmark.png" alt="scrap">
+              <!-- <i class="far fa-bookmark"></i> -->
+              <h3>스크랩한 글</h3>
+            </div>
+          </div>
+        </div>
+        <div class="myrecipe">
+          <h3 class="myrecipe-title">내 레시피</h3>
+          <v-container class="grey lighten-5" style="min-height: 0; padding: 10px" >
+              <v-row class="" no-gutters style="padding: 0;">
+                <v-col class="" cols="4">
+                  <img src="" alt="">
+                </v-col>
+                <v-col class="" cols="4">
+                  <img src="" alt="">
+                </v-col>
+                <v-col class="" cols="4">
+                  <img src="" alt="">
+                </v-col>
+              </v-row>
+            </v-container>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import "../../components/css/user.scss";
+
+export default {
+
+}
+</script>
