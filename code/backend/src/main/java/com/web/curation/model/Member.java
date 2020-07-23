7fd437249de840ad511ea62c3f62eeb0cb2c432f
김낙현 <sonata0226@gmail.com>
2020-07-23 08:40:15 +0000

    Feature/login/be

---
+++ code/backend/src/main/java/com/web/curation/model/Member.java
@@ -0,0 +1,107 @@
+// 하단 DB 설정 부분은 Sub PJT II에서 데이터베이스를 구성한 이후에 주석을 해제하여 사용.
+
+package com.web.curation.model;
+
+import lombok.Data;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.validation.constraints.NotEmpty;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.format.annotation.DateTimeFormat;
+
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@Entity
+@Getter
+@Setter
+public class Member {
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long no;
+
+	@NotEmpty
+	private String email;
+
+	@NotEmpty
+	private String password;
+
+	private String nickname;
+
+	private String address;
+
+	@Column(name = "create_date")
+	@DateTimeFormat(pattern = "yyyy-MM-dd")
+	@CreationTimestamp
+	private LocalDateTime create_date;
+	
+	public Member() {
+	};
+	
+	public Member(@NotEmpty String email, @NotEmpty String password, String nickname, String address) {
+		super();
+		this.email = email;
+		this.password = password;
+		this.nickname = nickname;
+		this.address = address;
+	}
+
+	public Long getNo() {
+		return no;
+	}
+
+	public void setNo(Long no) {
+		this.no = no;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getNickname() {
+		return nickname;
+	}
+
+	public void setNickname(String nickname) {
+		this.nickname = nickname;
+	}
+
+	public String getAddress() {
+		return address;
+	}
+
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+	public LocalDateTime getCreate_date() {
+		return create_date;
+	}
+
+	public void setCreate_date(LocalDateTime create_date) {
+		this.create_date = create_date;
+	}
+
+}
