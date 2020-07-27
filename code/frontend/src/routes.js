b1256a3b7d26fb0bdcc1faf40b60b089f15ad1c6
bu031015080@gmail.com <bu031015080@gmail.com>
2020-07-27 17:07:58 +0900

    S03P12B301-51

--- code/frontend/src/routes.js
+++ code/frontend/src/routes.js
@@ -8,6 +8,8 @@ import MarketPlace from "./views/store/Marketplace.vue";
 import GroupBuying from "./views/store/Groupbuying.vue";
 import SearchPassword from "./components/common/SearchPassword.vue";
 import CheckCertification from "./components/common/CheckCertification";
+import Main from "./views/Main.vue";
+import MyRef from "./views/user/MyRef.vue";
 
 export default [
   {
@@ -59,5 +61,15 @@ export default [
     path: "/store/groupbuying",
     name: "GroupBuying",
     component: GroupBuying,
+  },
+  {
+    path: "/Main",
+    name: "Main",
+    component: Main,
+  },
+  {
+    path: "/MyRef",
+    name: "MyRef",
+    component: MyRef,
   },
 ];
