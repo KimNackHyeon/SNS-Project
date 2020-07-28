7c5f0355f8f832cba1078b536bff7b15a2c90d6d
rjsgh1232 <rjsgh1232@koreatech.ac.kr>
2020-07-28 14:14:47 +0900

    S03P12B301-16 / Add signup rule page & daum postcode api with vuetify

--- code/frontend/src/App.vue
+++ code/frontend/src/App.vue
@@ -1,13 +1,13 @@
 <template>
   <v-app id="app">
-    <router-view @login="onLogin" @signup="onLogin"></router-view>
+    <router-view @login="onLogin" @signup="onSignup"></router-view>
   </v-app>
 </template>
 <script>
 import "./components/css/style.scss";
 import axios from "axios";
 
-const SERVER_URL = "http://127.0.0.1:9999";
+const SERVER_URL = "http://127.0.0.1:9999/food/api";
 
 export default {
   name: "app",
@@ -23,11 +23,12 @@ export default {
         password: password,
       };
       console.log(loginData);
-      axios.post(`${SERVER_URL}/account/login`, loginData)
+      axios
+        .post(`${SERVER_URL}/account/login`, loginData)
         .then((response) => {
           console.log(response);
-          this.$cookies.set("auth-token", response.data.key);
-          this.isLoggedIn = true
+          this.$cookies.set("auth-token", response.data);
+          this.isLoggedIn = true;
           this.$router.push("/home");
         })
         .catch((error) => {
@@ -39,9 +40,9 @@ export default {
       axios
         .post(`${SERVER_URL}/account/signup`, signupData)
         .then((response) => {
-          this.$cookies.set("auth-token", response.data.key);
+          this.$cookies.set("auth-token", response.data);
           this.isLoggedIn = true;
-          this.$router.push("/");
+          this.$router.push("/home");
         })
         .catch((error) => {
           console.log(error.response);
