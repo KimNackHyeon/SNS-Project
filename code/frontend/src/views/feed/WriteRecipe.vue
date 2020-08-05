ac15ed8aebb203d622939e920864e418547959ef
bu031015080@gmail.com <bu031015080@gmail.com>
2020-08-05 11:40:29 +0900

    S03P13B301-26 / Add WriteFeed and addFood pages

--- code/frontend/src/views/feed/WriteRecipe.vue
+++ code/frontend/src/views/feed/WriteRecipe.vue
@@ -1,19 +1,39 @@
 <template>
     <div style="height:100%; width:100%;">
-        <div style="width:100%; height:40px; border-top: 1px solid rgba(128, 128, 128, 0.15); border-bottom: 1px solid rgba(128, 128, 128, 0.15); text-align:center; margin-bottom:10px; ">
-            <router-link to="/feed/main">
-                <div style="width:50px; height:100%;border-right: 1px solid rgba(128, 128, 128, 0.15); float:left;">
-                    <v-icon size="30px" style="padding:6px 0px;">mdi-chevron-left</v-icon>
-                </div>
-            </router-link>
-            <div style="width:270px;height:100%; text-align:center; float:left; padding-top:8px;">
-            <h4>글쓰기</h4>
-            </div>
-            <div style="width:40px; height:100%; float:left;">
-            </div>
+      <div style="width:100%; height:40px; border-top:1px solid #0000001f; border-bottom: 1px solid #0000001f;">
+        <router-link to="/feed/main">
+        <div style="width:50px; height:100%; border-right:1px solid #0000001f; float:left; padding:4px 6px;">
+          <v-icon size="35px">mdi-chevron-left</v-icon>
         </div>
-        <div style="height:400px; width:100%;margin:auto;">
-         <Vueditor></Vueditor>
+        </router-link>
+        <div style="width:260px; height:100%; float:left; text-align:center; padding-top:8px;">
+          <h4> 레시피 작성</h4>
+        </div>
+      </div>  <!-- end of header -->
+
+        <div style="width:100%; height:0px; border-top: 1px solid rgba(128, 128, 128, 0.15); border-bottom: 1px solid rgba(128, 128, 128, 0.15); text-align:center; margin-bottom:10px; margin-top:15px;">
+           <div id="app">
+  <p><a href="https://github.com/ahastudio/CodingLife/tree/master/20170406/vuejs" target="_blank">https://github.com/ahastudio/CodingLife/tree/master/20170406/vuejs</a></p>
+  <carousel-component>
+    <carousel-item>
+      <h2>#1</h2>
+      <img src="https://octodex.github.com/images/original.png" alt="">
+    </carousel-item>
+    <carousel-item>
+      <h2>#2</h2>
+      <img src="https://octodex.github.com/images/octobiwan.jpg" alt="">
+    </carousel-item>
+    <carousel-item>
+      <h2>#3</h2>
+      <img src="https://octodex.github.com/images/daftpunktocat-thomas.gif" alt="">
+    </carousel-item>
+    <carousel-item>
+      <h2>#4</h2>
+      <img src="https://octodex.github.com/images/adventure-cat.png" alt="">
+    </carousel-item>
+  </carousel-component>
+</div>
+
         </div>
     </div>
 </template>
@@ -27,43 +47,97 @@ import 'vueditor/dist/style/vueditor.min.css'
 
 // your config here
 
-let config = {
-  toolbar: [
-    'removeFormat', 'undo', '|', 'elements', 'fontName', 'fontSize', 'foreColor', 'backColor', 'divider',
-    'bold', 'italic', 'underline', 'strikeThrough', 'links', 'divider',
-    'divider', 'justifyLeft', 'justifyCenter', 'justifyRight', 'justifyFull', 'indent', 'outdent',
-    'insertOrderedList', 'insertUnorderedList', '|', 'picture', 'tables', '|', 'switchView'
-  ],
-  fontName: [
-    {val: 'arial black'}, 
-    {val: 'times new roman'}, 
-    {val: 'Courier New'}
-  ],
-  fontSize: ['12px', '14px', '16px', '18px', '0.8rem', '1.0rem', '1.2rem', '1.5rem', '2.0rem'],
-  uploadUrl: ''
-};
-
 Vue.use(Vuex);
-Vue.use(Vueditor, config);
     
 // create a root instance
-new Vue({
-  el: '#editorContainer'
-});
 export default {
-}
+    data () {
+      return {
+        items: [
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/squirrel.jpg',
+          },
+          {
+            src: 'https://cdn.vuetifyjs.com/images/carousel/sky.jpg',
+          },
+          {
+            src: '',
+          },
+          {
+            src: '',
+          },
+          {
+            src: '',
+          },
+          {
+            src: '',
+          },
+          {
+            src: '',
+          },
+          {
+            src: '',
+          },
+        ],
+      }
+    },
+  }
 </script>
 
 <style scoped>
-.vueditor{
-    margin-right: 10px;
-    margin-left: 10px;
+#app {
+  margin: 10px auto;
+  width: 80%;
 }
-._2T9WD2D47RUUwZfCP40fYJ_0{
- width:300px;
+
+.carousel-component {
+  position: relative;
+  overflow: hidden;
 }
-img{
-    width:200px;
-    height:200px;
+
+.carousel-page {
+  visibility: hidden;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  transition: 0.5s;
+  padding: 10px;
+  border: 1px solid #EEE;
+}
+.carousel-page  h2 {
+    margin: 0;
+  }
+
+.carousel-page  img {
+    width: 300px;
+  }
+
+
+.carousel-page.active {
+  visibility: visible;
+  position: static;
+}
+
+.carousel-nav-prev,
+.carousel-nav-next {
+  position: absolute;
+  top: 50%;
+  transform: translate(0, -50%);
+  display: block;
+  width: 50px;
+  height: 50px;
+  border: 0;
+  background: rgba(0, 0, 0, 0.5);
+  color: #FFF;
+  cursor: pointer;
+}
+
+.carousel-nav-prev {
+  left: 5px;
+}
+
+.carousel-nav-next {
+  right: 5px;
 }
 </style>
