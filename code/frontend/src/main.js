e9b801b31f96ffefb9d71baaede2223d3060de13
김낙현 <sonata0226@gmail.com>
2020-08-20 05:45:16 +0000

    S03P13B301-115 / Add route.js

--- code/frontend/src/main.js
+++ code/frontend/src/main.js
@@ -17,6 +17,7 @@ Vue.use(VueGeolocation);
 Vue.use(VueMoment);
 
 import * as VueGoogleMaps from 'vue2-google-maps';
+import Swal from "sweetalert2";
 
 Vue.use(VueGoogleMaps, {
   load:{
@@ -28,6 +29,43 @@ const router = new VueRouter({
   mode: 'history',
   routes,
 });
+
+// router.beforeEach((to, from, next) => {
+
+//   if(from.path == "/" || to.path == "/"){
+//     if (Vue.$cookies.isKey('auth-token')) {
+//       next('/main')
+//     }
+//     else {
+//       next()
+//     }
+//   }
+//   else{
+//     if (Vue.$cookies.isKey('auth-token')) {
+//       // next()
+//       console.log('로그인했음');
+//     }
+//     else {
+//       next('/');
+//       // Swal.fire({
+//       //   title: '로그인 해주세요!',
+//       //   text: "",
+//       //   icon: 'warning',
+//       //   showCancelButton: true,
+//       //   confirmButtonColor: '#3085d6',
+//       //   cancelButtonColor: '#d33',
+//       //   confirmButtonText: '로그인 하기',
+//       //   cancelButtonText:'안 하기'
+//       // }).then((result) => {
+//       //   if (result.value) {
+//       //     next('/');
+//       //   }
+//       // })
+//       console.log('로그인안했음');
+//       // next('/error')
+//     }
+//   }
+// });
 
 new Vue({
   router,
--- code/frontend/src/views/error.vue
+++ code/frontend/src/views/error.vue
@@ -3,16 +3,20 @@
       <div style="width:80%; height:192px; margin:auto; margin-top: 156px;">
           <img src="../assets/images/ERROR.png" style="width:100%;">
             <h1 style="margin: 4px 0px 12px 0px;">에러가 발생했어요!</h1>
-            <router-link to="/">
-            <v-btn width="100%" color="rgb(160,212,105)">돌아가기</v-btn>
-            </router-link>
+            <!-- <router-link to="/"> -->
+              <v-btn width="100%" color="rgb(160,212,105)" @click="goback">돌아가기</v-btn>
+            <!-- </router-link> -->
       </div>
   </div>
 </template>
 
 <script>
 export default {
-
+  methods:{
+    goback(){
+      this.$router.go(-1);
+    }
+  }
 }
 </script>
 
